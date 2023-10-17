06eff456d6b0a0446a8b6189cdf42f492d8390cd
angevaldd <angevaldddje@gmail.com>
2023-10-17 09:55:12 +0000

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+
+        $categories = Category::factory(5)->create();
+        User::factory(10)->create()->each(function ($user) use ($categories) {
+            Post::factory(rand(1,4))->create([
+                'user_id' => $user->id,
+                'category_id' => ($categories->random(1)->first())->id
+            ]);
+        });
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
